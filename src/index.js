073530e921bfb6fd236f59e3b1de3e7809adb247
Gonçalo Reais <goncalorsd@gmail.com>
2021-02-06 18:43:19 +0000

    feat: creates simple express app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.use((req, res) => res.send('Test!'));
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log('Server is running at port ' + port + '!');
+});
