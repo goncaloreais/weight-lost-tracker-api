575de8fd511fa85bbb6a09f7e468e01ab3a504f5
Gonçalo Reais <goncalorsd@gmail.com>
2021-02-06 20:12:11 +0000

    chore: adds connection to the database

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const secrets = require('../secrets.json');
 
 const app = express();
 app.use(express.json());
+
+mongoose.connect(process.env.MONGO_DB_URI || secrets.mongo_uri);
+const db = mongoose.connection;
+
+if(!db) {
+    console.log("Error connecting db");
+}
+else {
+    console.log("Db connected successfully");
+}
 
 app.use((req, res) => res.send('Test!'));
 
