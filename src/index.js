d1757c5bd2407ca1b8b3293765c88065e7d0e748
Gonçalo Reais <goncalorsd@gmail.com>
2021-02-06 19:47:16 +0000

    fix: changes the default port for app to run

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 
 app.use((req, res) => res.send('Test!'));
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('Server is running at port ' + port + '!');
 });
