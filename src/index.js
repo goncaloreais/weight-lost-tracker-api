4c0e0e033f43418bb1a986905fce0e48e583fd6a
Gonçalo Reais <goncalorsd@gmail.com>
2021-02-06 20:31:18 +0000

    docs: adds comments to the file

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
+// dependencies
 const express = require('express');
 const mongoose = require('mongoose');
-let secrets = null;
 
+// adds reference to secrets file when running locally
+let secrets = null;
 if(!process.env.ON_HEROKU) {
     secrets = require('../secrets.json');
 }
 
+// app init
 const app = express();
 app.use(express.json());
 
+// db connection init
 mongoose.connect(
     process.env.MONGO_DB_URI || 
     secrets.mongo_uri, { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
 const db = mongoose.connection;
-
-if(!db) {
-    console.log("Error connecting db");
-}
-else {
-    console.log("Db connected successfully");
+if(!db) { 
+    console.log("Error connecting to the db!"); 
 }
 
 // default error
@@ -30,6 +30,7 @@ app.use((req, res) => res.status(404).send({
     status: 'error'
 }));
 
+// serving the app
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('Server is running at port ' + port + '!');
