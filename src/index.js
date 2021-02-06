5a49aa27a729773290427469ef50931fd75bf178
Gonçalo Reais <goncalorsd@gmail.com>
2021-02-06 20:52:12 +0000

    fix: fixes wrong import route

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 // required imports
-const routes = require('./app/routes');
+const routes = require('./routes');
 
 // adds reference to secrets file when running locally
 let secrets = null;
