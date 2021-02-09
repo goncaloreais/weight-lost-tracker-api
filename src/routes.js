28a0c4c90a4e1af5cd62214e281f71e9760afe08
Gonçalo Reais <goncalorsd@gmail.com>
2021-02-09 23:20:28 +0000

    feat: adds routes to manipulate users

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,17 @@ const router = require('express').Router();
 
 // importing controllers
 const weightLogCtrl = require('./controllers/weightLog');
+const usersCtrl = require('./controllers/users');
 
 // manipulating Log model
+// @TODO: change this route so it returns a user weight-logs 
 router.route('/weight-log')
     .get(weightLogCtrl.get)
     .post(weightLogCtrl.post);
+
+router.route('/users')
+    .get(usersCtrl.get)
+    .post(usersCtrl.post);
 
 // exports routes
 module.exports = router;
