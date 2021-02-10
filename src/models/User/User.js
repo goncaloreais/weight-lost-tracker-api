bb2cae4c9b46bad4b1ed5cf4d4b0c4bbc91fcc13
Gonçalo Reais <goncalorsd@gmail.com>
2021-02-10 21:59:15 +0000

    feat: adds username and password to User model with encription

--- src/models/User/User.js
+++ src/models/User/User.js
@@ -1,16 +1,41 @@
 // importing dependencies
 const mongoose = require('mongoose');
 
+// @TODO: more on this: https://www.npmjs.com/package/bcrypt
+const bcrypt = require('bcryptjs');
+
 // creating the schema
 const UserSchema = mongoose.Schema({
     name: String,
     surname: String,
+    username: {
+        type: String,
+        required: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        lowercase: true,
+        minlength: 6,
+        trim: true,
+    },
     initialWeight: Number,
     currentWeight: Number,
     weightUnit: String,
     height: Number,
     sessionToken: String,
     accessToken: String,
+});
+
+UserSchema.pre('save', async function(next) {
+    const user = this;
+    
+    if(user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    
+    next();
 });
 
 // creating the model
