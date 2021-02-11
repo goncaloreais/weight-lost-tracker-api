7c1f64ee08424ce0d44b64bbff5839d8619b59fa
Gonçalo Reais <goncalorsd@gmail.com>
2021-02-11 23:19:49 +0000

    feat: adds Login model to store active sessions

---
+++ src/models/Login/Login.js
@@ -0,0 +1,18 @@
+// importing dependencies
+const mongoose = require('mongoose');
+
+// creating the schema
+const LoginSchema = mongoose.Schema({
+    sessionToken: String,
+    userId: {
+        type: Schema.Types.ObjectId, 
+        ref: 'User'
+    }
+    
+});
+
+// creating the model
+const Login = mongoose.model('login', LoginSchema);
+
+// exporting
+module.exports = Login;
