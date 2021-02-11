8a1da5bbbb039b54cf2bec0437b6ad809422a3fe
Gonçalo Reais <goncalorsd@gmail.com>
2021-02-11 23:09:53 +0000

    feat: adds login logic

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,7 @@
 // import required dependencies
 const bcrypt = require('bcryptjs');
+const tokenGenerator = require('../utils/tokenGenerator');
+const httpResponse = require('../utils/httpResponses');
 
 // imports required models
 const User = require('../models/User/User');
@@ -7,17 +9,34 @@ const User = require('../models/User/User');
 // login request
 function post(req, res) {
     User.findOne({ username: req.body.username }, (error, user) => {
-        console.log(user);
         if(!user) {
             // @TODO: review this
             res.json('no_user');
             return;
         }
 
+        console.log('ecgou aqui: ', req.body.password, user.password);
         if(bcrypt.compareSync(req.body.password, user.password)) {
+            user.sessionToken = tokenGenerator.token();
+            user.lastAccess = Date.now();
             
-        } else {
+            // @TODO: handle error
+            user.save((err, newUser) => {
+                // @TODO: review this
+                
+                // removes sensible info from response
+                delete newUser.password;
+                delete newUser.__v;
+                delete newUser._id;
 
+                res.status(200).send(
+                    httpResponse.successResponse(200, 'User authenticated!', newUser)
+                );
+            })
+        } else {
+            res.status(401).send(
+                httpResponse.successResponse(401, 'User not authenticated!')
+            );
         }
     });
 };
