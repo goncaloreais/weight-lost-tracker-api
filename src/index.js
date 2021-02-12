5cb993505073f71dd5ce1447ff89142a45e0042c
Gonçalo Reais <goncalorsd@gmail.com>
2021-02-12 00:20:53 +0000

    fix: adds logic to validate the request token and its expire time

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ const mongoose = require('mongoose');
 // required imports
 const utils = require('./utils/httpResponses');
 const routes = require('./routes');
+const Login = require('./models/Login/Login');
 
 // adds reference to secrets file when running locally
 let secrets = null;
 if(!process.env.production) {
     secrets = require('../secrets.json');
 }
+
+// global vars for app
+// 30 min * 60 secs * 1000 ms
+const tokenExpiration = 30 * 60 * 1000;
 
 // app init
 const app = express();
@@ -27,15 +32,39 @@ if(!db) {
     console.log("Error connecting to the db!"); 
 }
 
-// adds some security (temporary)
+// validates token
 app.use((req, res) => {
-    const header = process.env.production ? process.env.security_header : secrets.security_header;
-    if(req.headers.security !== header) {
-        res.status(401).send(utils.errorResponse(401, 'Unauthorized!'));
-        return;
-    }
+    // /auth is the only request that do not requires a token
+    if(req.originalUrl !== '/auth') {
 
-    req.next();
+        // if there is no token in the request
+        if(!req.query.token) {
+            res.status(401).send(utils.errorResponse(401, 'Unauthorized!'));
+            return;
+        }
+
+        Login.findOne({ sessionToken: req.query.token }, (error, session) => {
+            // if the request token is not found
+            if(!session) {
+                res.status(401).send(utils.errorResponse(401, 'Unauthorized!'));
+                return;
+            }
+
+            // if the token is found but it's expired
+            if(Date.now() - session.lastAccess >= tokenExpiration) {
+                res.status(401).send(utils.errorResponse(401, 'Session expired!'));
+                return;
+            }
+
+            // if the token is found and is not expired
+            session.lastAccess = Date.now();
+            session.save((error, newSession) => {
+                req.next();
+            });
+        });
+    } else {
+        req.next();
+    }
 })
 
 // routes usage
