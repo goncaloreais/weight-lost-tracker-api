ee137437d68f9445860411e1c29d7324603c2114
Gonçalo Reais <goncalorsd@gmail.com>
2021-02-12 00:24:27 +0000

    feat: adds logic to generate sessions on login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -16,37 +16,57 @@ function post(req, res) {
             return;
         }
 
-        console.log('ecgou aqui: ', req.body.password, user.password);
+        // compares the received password with the hashed one in the db
         if(bcrypt.compareSync(req.body.password, user.password)) {
+
+            // generates a new token for the logged user
             user.sessionToken = tokenGenerator.token();
             
             // @TODO: handle error
-            user.save((err, newUser) => {
+            user.save((userError, newUser) => {
                 // @TODO: review this
 
+                // since a new token is generated, the session must be updated or created
                 let login = new Login({
                     sessionToken: newUser.sessionToken,
                     lastAccess: Date.now(),
                     userId: newUser._id,
                 });
-                    
-                Login.create(login, (err) => {
-                    if (err) {
+                
+
+                Login.findOne({ userId: newUser._id }, (sessionError, sesssion) => {
+                    // removes sensible info from response
+                    delete newUser.password;
+                    delete newUser.__v;
+                    delete newUser._id;
+
+                    if (sessionError) {
                         // @TODO: handle error 
                         res.json(err);
                     } else {
-                        // removes sensible info from response
-                        delete newUser.password;
-                        delete newUser.__v;
-                        delete newUser._id;
+                        // if there is no session, creates one
+                        if(!sesssion) {
+                            Login.create(login, (newSessionError, newSession) => {
+                                res.status(200).send(
+                                    httpResponse.successResponse(200, 'User authenticated!', newUser)
+                                );
+                            })
+                        } else {
+                            // if there is one, updates it
+                            sesssion.sessionToken = login.sessionToken;
+                            sesssion.lastAccess = login.lastAccess;
 
-                        res.status(200).send(
-                            httpResponse.successResponse(200, 'User authenticated!', newUser)
-                        );
+                            sesssion.save((newSessionError, newSession) => {
+                                res.status(200).send(
+                                    httpResponse.successResponse(200, 'User authenticated!', newUser)
+                                );
+                            })
+                        }
                     }
                 });
-            })
+            });
         } else {
+            // if the password is incorrect
             res.status(401).send(
                 httpResponse.successResponse(401, 'User not authenticated!')
             );
